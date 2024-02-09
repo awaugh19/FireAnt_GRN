916e6172f5453dd58a72a527142e70b6a42d702e
awaugh19 <50500978+awaugh19@users.noreply.github.com>
2024-02-08 22:29:42 -0500

    Update 4.3_LK_SB_2p.sh

--- 4.3_LK_SB_2p.sh
+++ 4.3_LK_SB_2p.sh
@@ -1,66 +1,69 @@
 #!/bin/bash
-#SBATCH --job-name=LK_SB_B_mBB_2p.sh                                  #Job name
-#SBATCH --partition=batch		                                        #Partition (queue) name
-#SBATCH --ntasks=1			                                            #Single task job
-#SBATCH --cpus-per-task=8	                                          #Number of cores per task
-#SBATCH --mem=32gb			                                            #Total memory for job
-#SBATCH --time=96:00:00  		                                        #Time limit hrs:min:sec
-#SBATCH --output=/home/ahw22099/Alignment_to_UNIL_3.4/LK_SB_B_mBB_2p.log.%j			    #Standard output
-#SBATCH --error=/home/ahw22099/Alignment_to_UNIL_3.4/LK_SB_B_mBB_2p.err.%j			    #Standard error log
-#SBATCH --mail-user=ahw22099@uga.edu                                #Where to send mail -
-#SBATCH --mail-type=END,FAIL                                        #Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --job-name=4.3_LK_SB_2p.sh                                               #Job name
+#SBATCH --partition=batch		                                                    #Partition (queue) name
+#SBATCH --ntasks=1			                                                        #Single task job
+#SBATCH --cpus-per-task=10                                                      #Number of cores per task
+#SBATCH --mem=24gb			                                                        #Total memory for job
+#SBATCH --time=72:00:00  		                                                    #Time limit hrs:min:sec
+#SBATCH --output=/scratch/ahw22099/FireAnt_GRN/std_out/4.3_LK_SB_2p.log.%j			#Standard output
+#SBATCH --error=/scratch/ahw22099/FireAnt_GRN/std_out/4.3_LK_SB_2p.err.%j		    #Standard error log
+#SBATCH --mail-user=ahw22099@uga.edu                                            #Where to send mail -
+#SBATCH --mail-type=END,FAIL,ARRAY_TASKS                                        #Mail events (BEGIN, END, FAIL, ALL)
+#SBATCH --array=0-52
 
+################## STAR ##################
+module load STAR/2.7.10b-GCC-11.3.0
+########### SB ############
 
-LK_trimmed_fq="/scratch/ahw22099/UNIL_3.4_Alignment/LK_trimmed_fq"
+LK_trimmed_fq="/scratch/ahw22099/FireAnt_GRN/LK_trimmed_fq"
 if [ ! -d $LK_trimmed_fq ]
 then
 mkdir -p $LK_trimmed_fq
 fi
 
-STAR_genome_SB="/scratch/ahw22099/UNIL_3.4_Alignment/LK_STAR_genome_SB"
+STAR_genome_SB="/scratch/ahw22099/FireAnt_GRN/STAR_genome_SB"
 if [ ! -d $STAR_genome_SB ]
 then
 mkdir -p $STAR_genome_SB
 fi
 
 
-SB_genome="/scratch/ahw22099/UNIL_3.4_Alignment/UNIL_Sinv_3.4_SB"
+SB_genome="/scratch/ahw22099/FireAnt_GRN/UNIL_Sinv_3.4_SB"
 if [ ! -d $SB_genome ]
 then
 mkdir -p $SB_genome
 fi
 
-################## STAR ##################
-module load STAR/2.7.10b-GCC-11.3.0
-
-##2nd pass
-LK_STAR_SB="/scratch/ahw22099/UNIL_3.4_Alignment/LK_STAR_SB"
-if [ ! -d $STAR_SB ]
+##1st pass
+LK_STAR_SB="/scratch/ahw22099/FireAnt_GRN/LK_STAR_SB"
+if [ ! -d $LK_STAR_SB ]
 then
-mkdir -p $STAR_SB
+mkdir -p $LK_STAR_SB
 fi
 
-FirstPass_SB="/scratch/ahw22099/UNIL_3.4_Alignment/LK_STAR_SB/1p_out"
+FirstPass_SB="/scratch/ahw22099/FireAnt_GRN/LK_STAR_SB/1p_out"
+if [ ! -d $FirstPass_SB ]
+then
+mkdir -p $FirstPass_SB
+fi
 
-SecondPass_SB="/scratch/ahw22099/UNIL_3.4_Alignment/LK_STAR_SB/2p_out"
+SecondPass_SB="/scratch/ahw22099/FireAnt_GRN/LK_STAR_SB/2p_out"
 if [ ! -d $SecondPass_SB ]
 then
 mkdir -p $SecondPass_SB
 fi
 
 cd $LK_trimmed_fq
-for base in \
-B_mBB_1 \
-B_mBB_2 \
-B_mBB_3 \
-B_mBB_4 \
-B_mBB_5_merge \
-B_mBB_6_merge \
-B_mBB_7
+#make sample list for array job
+R1_sample_list=($(<LK_trimmed_input_list_1.txt))
+R2_sample_list=($(<LK_trimmed_input_list_2.txt))
+R1=${R1_sample_list[${SLURM_ARRAY_TASK_ID}]}
+R2=${R2_sample_list[${SLURM_ARRAY_TASK_ID}]}
 
-do
-  R1=$LK_trimmed_fq/${base}.R1_val_1.fq.gz
-  R2=$LK_trimmed_fq/${base}.R2_val_2.fq.gz
+echo $R1
+echo $R2
+
+base=`basename "$R1" .R1_val_1.fq.gz`
 
 STAR \
 --readFilesCommand zcat \
@@ -81,14 +84,21 @@ STAR \
 --outSAMattrIHstart 0 \
 --outFileNamePrefix $SecondPass_SB/"$base".SB2pass. \
 --limitBAMsortRAM 30000000000 \
---sjdbFileChrStartEnd \
-$FirstPass_SB/B_mBB_1.SB1pass.SJ.out.tab \
+--sjdbFileChrStartEnd $FirstPass_SB/B_mBB_1.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_2.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_3.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_4.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_5_merge.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mBB_6.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_6_merge.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_mBB_7.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_10.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_11_merge.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_12.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_13_merge.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_14_merge.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_15.SB1pass.SJ.out.tab \
+$FirstPass_SB/B_mpBB_16.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_pBB_1.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_pBB_2.SB1pass.SJ.out.tab \
 $FirstPass_SB/B_pBB_3.SB1pass.SJ.out.tab \
@@ -109,6 +119,12 @@ $FirstPass_SB/G_mBB_5.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_mBB_6.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_mBB_7.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_mBB_8.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_10.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_11.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_13.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_14.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_15.SB1pass.SJ.out.tab \
+$FirstPass_SB/G_mpBB_17.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBB_1.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBB_3.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBB_5.SB1pass.SJ.out.tab \
@@ -121,4 +137,3 @@ $FirstPass_SB/G_pBL_5.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBL_6.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBL_7.SB1pass.SJ.out.tab \
 $FirstPass_SB/G_pBL_8.SB1pass.SJ.out.tab
-done
