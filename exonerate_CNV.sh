e6a1d4b1768277a47da987a78833cfcadd0be578
awaugh19 <50500978+awaugh19@users.noreply.github.com>
2024-05-02 11:08:03 -0400

    Create exonerate_CNV.sh

---
+++ exonerate_CNV.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=exonerate_CNV.sh
+#SBATCH --partition=batch
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=24gb
+#SBATCH --time=24:00:00
+#SBATCH --output=/scratch/ahw22099/FireAnt_GRN/std_out/exonerate_CNV.log.%j
+#SBATCH --error=/scratch/ahw22099/FireAnt_GRN/std_out/exonerate_CNV.err.%j
+#SBATCH --mail-user=ahw22099@uga.edu
+#SBATCH --mail-type=END,FAIL
+
+# Load necessary modules
+module load Exonerate/2.4.0-GCC-12.2.0
+
+# Define variables
+exonerate --model est2genome /scratch/ahw22099/FireAnt_GRN/Fontana2020_CNV/Sb-vs-SB_CNV_genes_Sinv.fasta \
+/scratch/ahw22099/FireAnt_GRN/UNIL_Sinv_3.4_SB/GCF_016802725.1_UNIL_Sinv_3.0_genomic.fna
